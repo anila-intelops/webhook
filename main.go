53633398f53ad39133e7828ff659aa435e3e9510
an1l4 <1995anila@gmail.com>
2023-10-11 12:49:06 +0530

    sf

--- main.go
+++ main.go
@@ -4,12 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
-	fmt.Println("anila")
-	fmt.Println("hi")
-	fmt.Println("how are u")
-	fmt.Println("anila")
-	fmt.Println("anila")
-	fmt.Println("hi")
-	fmt.Println("how are u")
-	fmt.Println("anila")
+
 }
