85efe7c9d1afa84d8228bd642f132560335fe01b
an1l4 <1995anila@gmail.com>
2024-02-21 13:55:44 +0530

    re

--- main.go
+++ main.go
@@ -7,11 +7,5 @@ func main() {
 	fmt.Println("hello")
 	fmt.Println("hello")
 	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
-	fmt.Println("hello")
 
 }
